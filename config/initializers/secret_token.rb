93b377ba3b0dded4b777c2aefa73883f27f0a482
nsrivast <admin@SoBe-Laptop.local>
2013-05-14 17:02:35 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mywines::Application.config.secret_token = '3784ea5e40f28672220ced2e2dfefee5f9e726908397753264e053abafdccd1fdcbcfbfbb97e8704646d0dacb0cc89d566d49b403ae22c12f99bf79711656988'
