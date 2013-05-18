1605c92278eb13aa8e1c4efd1a79ca2b3e49b4a9
nsrivast <nsrivast@gmail.com>
2013-05-18 12:24:32 -0400

    modified charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,25 +1,61 @@
 class ChartsController < ApplicationController
   
   def all
-    @charts = %w{rating_v_price price_v_time rating_v_time}
-  end
-
-  def rating_v_price
-    @chart_data = Wine.all.collect{ |w| {:name => w.name, :scatter => [[w.price, w.avg_rating]] }}
-  end
-  def price_v_time
     epoch_date = Date.new(1970,1,1)
     user = User.find_by_id(1)
-    @chart_data = user.tastings.collect { |t| 
-      {:name => t.wine.name, :scatter => [[(t.drank_on - epoch_date).to_i, t.wine.price ]] }
-    }
-  end
-  def rating_v_time
-    epoch_date = Date.new(1970,1,1)
-    user = User.find_by_id(1)
-    @chart_data = user.tastings.collect { |t| 
-      {:name => t.wine.name, :scatter => [[(t.drank_on - epoch_date).to_i, t.wine.avg_rating ]] }
-    }
+    tastings = user.tastings
+    
+    @charts = [
+      {
+        :name => "rating_v_price",
+        :title => "Rating vs Price",
+        :subtitle => "all wines",
+        :datetime => false,
+        :x_axis => "Price ($)",
+        :y_axis => "Rating",
+        :data => Wine.all.collect{ |w| {
+          :name => w.name,
+          :click_name => w.pretty_name,
+          :id => w.id,
+          :x => w.price,
+          :y => w.avg_rating
+          }
+        }
+      },
+      {
+        :name => "price_v_time",
+        :title => "Price Timeline",
+        :subtitle => "all wines",
+        :datetime => true,
+        :x_axis => "Date Drank",
+        :y_axis => "Price",
+        :data => tastings.collect{ |t| {
+          :name => t.wine.name,
+          :click_name => t.wine.pretty_name,
+          :id => t.wine.id,
+          :x => t.drank_on,
+          :y => t.wine.price,
+          }
+        }
+      },
+      {
+        :name => "rating_v_time",
+        :title => "Rating Timeline",
+        :subtitle => "all wines",
+        :datetime => true,
+        :x_axis => "Date Drank",
+        :y_axis => "Rating",
+        :data => tastings.collect{ |t| {
+          :name => t.wine.name,
+          :click_name => t.wine.pretty_name,
+          :id => t.wine.id,
+          :x => t.drank_on,
+          :y => t.wine.avg_rating
+          }
+        }
+      }    
+    ]
+    
   end
 
 end
